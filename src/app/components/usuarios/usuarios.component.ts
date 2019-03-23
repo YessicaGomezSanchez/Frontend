6637829666b5cf4c24199081aa37e377f2b58961
HamiltonRenteria <Hamiltonrenteria@outlook.com>
2019-03-22 23:15:36 -0500

    Se sube mejora a la creación de Usuarios, actualmente se realiza una lista con los datos de la pantalla.

--- src/app/components/usuarios/usuarios.component.ts
+++ src/app/components/usuarios/usuarios.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-usuarios',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsuariosComponent implements OnInit {
 
+  userArray: Array<{
+    Documento: number,
+    Nombres: string,
+    Apellidos: string,
+    FechaNacimiento: Date,
+    Direccion: string,
+    TelefonoFijo: number,
+    Celular: number,
+    CorreoElectronico: string,
+    DataUsuario: Array<{
+      RolUsuario: string,
+      NombreUsuario: string,
+      Contraseña: string
+    }>
+  }> = new Array();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  saveUser(formUser: NgForm, formDataUser: NgForm) {
+    this.userArray.push(
+      formUser.value.docIdentidad
+      , formUser.value.nombre
+      , formUser.value.apellidos
+      , formUser.value.fechaNacimiento
+      , formUser.value.direccion
+      , formUser.value.telefonoFijo
+      , formUser.value.celular
+      , formUser.value.email
+      );
+    console.log(this.userArray);
   }
 
 }
