e72c8ed29261260142b41fddfa4600fb8d24a27b
yessica gomez <yegs95@gmail.com>
2019-04-24 23:35:22 -0500

    se creo el metodo para llenar la tabla de conductores

--- src/app/components/taxis/taxis.component.ts
+++ src/app/components/taxis/taxis.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { TaxisService } from '../../services/taxis.service';
+import {UsuariosService  } from '../../services/usuarios.service';
 import { enableProdMode } from '@angular/core';
 
 
@@ -12,12 +13,21 @@ enableProdMode();
   styleUrls: ['./taxis.component.css']
 })
 export class TaxisComponent implements OnInit {
-  constructor(private taxisService: TaxisService) { }
+  public nombres: string;
+  public apellidos: string;
+  public numero_celular: string;
+  public num_licencia: string;
+  public categoria: string;
+  public fecha_venc_licencia: Date;
 
+ 
+
+  constructor(private taxisService: TaxisService, private usuarioServicio:UsuariosService) { }
+  conductores=[];
   ngOnInit() {
   }
 
-  GuardarTaxi(form: NgForm) {
+  GuardarTaxi(form: NgForm, conductores: any[]) {
     const taxi = {
       modelo: form.value.modelo,
       placa: form.value.placa,
@@ -39,20 +49,33 @@ export class TaxisComponent implements OnInit {
       tipo_documento: form.value.tipo_documento,
       cedula: form.value.cedula,
       correo: form.value.correo,
-      conductores: []
-      // {
-      //   nombre_apellidosC: form.value.nombre_apellidosC,
-      //   tipo_documentoC: form.value.tipo_documentoC,
-      //   cedulaC: form.value.cedulaC,
-      //   correoC: form.value.correoC,
-      //   habilitado: true,
-      // }
+      conductores: [conductores]
     };
 
-    const datataxi = JSON.stringify(taxi);
-    console.log(datataxi);
-    this.taxisService.postTaxis(datataxi);
+    this.taxisService.postTaxis(taxi);
     form.reset();
   }
 
+  buscarUsuario(form: NgForm): void {
+    const id = form.value.cedulaC;
+    this.usuarioServicio.getUsuario(id).subscribe(data => {
+      if (data.rol =="Conductor") {
+      this.nombres = data.nombres;
+      this.apellidos = data.apellidos,
+      this.numero_celular = data.numero_celular,
+      this.num_licencia = data.num_licencia,
+      this.categoria = data.categoria,
+      this.fecha_venc_licencia = data.fecha_venc_licencia
+      } else {
+        //agregar las notificaciones
+        console.log('usuario no es un taxista', data.nombres)
+
+      }
+      
+    //  console.log('datos', data)
+    })
+  }
+  agregarConductores(data){
+    console.log('datos', data)
+  }
 }
