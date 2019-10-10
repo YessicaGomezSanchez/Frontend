2a1cf911cdb50cd04a11529a39066c0efdb4dc6b
Yessica Eleana Gómez Sánchez <ygomez@yuxiglobal.com>
2019-10-10 16:33:53 -0500

    cambios en el diseño de la pantalla principal

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,9 +4,27 @@ import { SesionService } from 'src/app/services/sesion.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  Texts = [
+    {
+      "name": "Usuarios",
+      "description": "Some quick example text to build on the card title and make up the bulk of the card's content.",
+      "image":"../../../assets/imagenes/user.png"
+    },
+    {
+      "name": "Acopio",
+      "description": "Some quick example text to build on the card title and make up the bulk of the card's content.",
+      "image":"../../../assets/imagenes/user.png"
+    },
+    {
+      "name": "Taxis",
+      "description": "Some quick example text to build on the card title and make up the bulk of the card's content.",
+      "image":"../../../assets/imagenes/user.png"
+    } 
+   
+  ];
 
   constructor(private sesionService: SesionService) {
     this.usuarioPrimeraVez();
--- src/app/components/nabvar/header/header.component.ts
+++ src/app/components/nabvar/header/header.component.ts
@@ -3,13 +3,40 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+
+  menu = [
+    {
+      "name": "Usuario",
+      "link": "/usuario",
+    },
+    {
+      "name": "Taxis",
+      "link": "/taxis",
+    },
+    {
+      "name": "Acopio",
+      "link": "/sesion",
+    }
+    ,
+    {
+      "name": "Historia",
+      "link": "/historia",
+    }
+    ,
+    {
+      "name": "Contacto",
+      "link": "/contacto",
+    }
+  ];
 
   constructor() { }
 
   ngOnInit() {
+    console.log('length: ', this.menu.length);
+    
   }
 
 }
